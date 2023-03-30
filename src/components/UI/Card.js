17c53bfbdd4faa13f70aff250d07655d3c9cc32c
jjguzme <JuanJose.Guzman@microfocus.com>
2023-03-30 14:55:51 -0600

    split components, reorganized and finished setion 3

---
+++ src/components/UI/Card.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Card.css";
+
+function Card(props){
+    const clases = 'card ' + props.className;
+
+    return <div className={clases}>{props.children}</div>
+}
+
+export default Card;
