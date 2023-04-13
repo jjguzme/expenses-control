1d057fc099ed7a34a553a30f5080c7acd2df1efc
jjguzme <JuanJose.Guzman@microfocus.com>
2023-04-13 15:15:51 -0600

    Modify function click handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,15 +4,17 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  let title = props.title;
+  
   const clickHandler = () => {
-    console.log("Clicked!!!!!")
+    title = 'Updated!';
   }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
